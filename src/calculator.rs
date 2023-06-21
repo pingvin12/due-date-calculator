70bdabf6f72abd63a3c412a4d4e10d4db78e2951
pingvin12 <fenyesj@gmail.com>
2023-06-21 19:39:53 +0200

    feat(calculator): add calculator

---
+++ src/calculator.rs
@@ -0,0 +1,62 @@
+use std::time::{SystemTime, UNIX_EPOCH, Duration};
+
+use crate::misc::{errortypes::calculation_error::CalculationError, datetime_utils::DatetimeUtils, datetime_utils::{EligibleDays, WORKDAY_LENGTH}};
+
+
+pub struct Calculator {
+    workday_length: i16,
+}
+
+impl Calculator {
+    // retrieve workday length thats static and store it for ease of access
+    pub fn new() -> Result<Self, CalculationError> {
+    Ok(Calculator { workday_length: WORKDAY_LENGTH.to_owned().try_into().unwrap() })
+    }
+
+    //main entry function used after validating input
+    pub fn calculate_due_date(&mut self, creation_date: SystemTime, turnaround_hours: i16) -> Result<SystemTime, CalculationError> {
+        let mut workdays = self.get_work_days(turnaround_hours);
+
+        let mut due_date = creation_date;
+        while workdays > 0 {
+            due_date = self.get_next_work_day(due_date)?;
+            workdays = workdays - 1;
+        }
+        // add hours_in_workday to due_date
+        let seconds = turnaround_hours as u64 * 60 * 60;
+        if seconds > 0 {
+            let duration = Duration::from_secs(seconds);
+            due_date = due_date + duration;
+            Ok(due_date)
+        } else {
+            Err(CalculationError::Unknown)
+        }
+    }
+
+    // When given a SystemTime, returns a Result containing an enum EligibleDays for readability
+    fn get_next_work_day(&mut self, datetime: SystemTime) -> Result<SystemTime, CalculationError> {
+        let day_of_week = DatetimeUtils::get_day_of_week(datetime);
+        if day_of_week == EligibleDays::Friday {
+            return Ok(DatetimeUtils::add_days(datetime, 3));
+        } else if day_of_week == EligibleDays::Saturday {
+            return Ok(DatetimeUtils::add_days(datetime, 2));
+        } else {
+            return Ok(DatetimeUtils::add_days(datetime, 1));
+        }
+    }
+
+    //Returns work days by subtracking the hours with workday_length. ex.: 16 / 2 = 8
+    fn get_work_days(&mut self, hours: i16) -> i16 {
+        return hours / self.workday_length;
+    }
+    
+}
+
+// Default implementation for calculator incase of getting rid of static inputs.
+impl Default for Calculator {
+    fn default() -> Self {
+        Calculator {
+            workday_length: 8,
+        }
+    }
+}
---
+++ tests/calculator_tests.rs
@@ -0,0 +1,21 @@
+#[cfg(test)]
+mod duedate_tests {
+    use due_date_calculator::{calculator::Calculator, misc::datetime_utils::DatetimeUtils};
+    use test_case::test_case;
+    use std::time::{Duration, SystemTime};
+
+    #[test_case(SystemTime::UNIX_EPOCH + Duration::from_secs(1667320800), 1, SystemTime::UNIX_EPOCH + Duration::from_secs(1667324400); "test 1: When calculate due date returns success")]
+    #[test_case(SystemTime::UNIX_EPOCH + Duration::from_secs(1667320800), 0, SystemTime::UNIX_EPOCH + Duration::from_secs(1667320800); "test 2: When calculate due date with zero turnaround hours")]
+    #[test_case(SystemTime::UNIX_EPOCH + Duration::from_secs(1667320800), 24, SystemTime::UNIX_EPOCH + Duration::from_secs(1667839200); "test 3: When calculate due date with one day turnaround")]
+    #[test_case(SystemTime::UNIX_EPOCH + Duration::from_secs(1667320800), 48, SystemTime::UNIX_EPOCH + Duration::from_secs(1668357600); "test 4: When calculate due date with two days turnaround")]
+    #[test_case(SystemTime::UNIX_EPOCH + Duration::from_secs(1667320800), 168, SystemTime::UNIX_EPOCH + Duration::from_secs(1670604000); "test 5: When calculate due date with one week turnaround")]
+    fn test_calculate_due_date(creation_date: SystemTime, turnaround_hours: i16, duedate: SystemTime) {
+        let mut calc = Calculator::new().unwrap();
+        let res = calc.calculate_due_date(creation_date, turnaround_hours);
+        if let Ok(value) = res {
+            let fmt = format!("{}", DatetimeUtils::format_system_time(value));
+            let fmt_expected = format!("{}", DatetimeUtils::format_system_time(duedate));
+            assert_eq!(fmt_expected, fmt);
+        }
+    }
+}
