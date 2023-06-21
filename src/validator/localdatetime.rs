f7654ea725384604e1a761df49932c6dd0496420
pingvin12 <fenyesj@gmail.com>
2023-06-21 19:41:06 +0200

    feat(datetime_validator): add datetime validation

---
+++ src/validator/localdatetime.rs
@@ -0,0 +1,20 @@
+use std::{time::SystemTime, str::FromStr};
+
+use chrono::{DateTime, Utc};
+
+use crate::misc::errortypes::validation_error::ValidationError;
+
+pub struct DatetimeValidator {
+}
+
+impl DatetimeValidator {
+    pub fn validate(datetime: &str) -> Result<SystemTime, ValidationError> {
+        
+        let var = match DateTime::<Utc>::from_str(datetime) {
+            Ok(system_time) => system_time,
+            Err(_) => return Err(ValidationError::InvalidDateSyntax { found: datetime.to_owned() }),
+        };
+
+        Ok(Into::<SystemTime>::into(var))
+    }
+}
---
+++ tests/validator/datetime_validator_tests.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod syntax_validator_tests {
+    use due_date_calculator::validator::localdatetime::DatetimeValidator;
+    use test_case::test_case;
+    use std::time::{Duration, SystemTime};
+
+    #[test_case("2023-06-20T10:30:00Z", SystemTime::UNIX_EPOCH + Duration::from_secs(1687257000), true; "Validate Datetime should return successfully with the expected SystemTime")]
+    #[test_case("2023-06-20T11:30:00Z", SystemTime::UNIX_EPOCH + Duration::from_secs(1687257000), false; "Validate Datetime should fail with the expected SystemTime")]
+    fn test_syntax_localdatetime_validator(given_datetime: &str, expected_datetime: SystemTime, success: bool) {
+        match success {
+            true => assert_eq!(expected_datetime, DatetimeValidator::validate(given_datetime).unwrap()),
+            false => assert_ne!(expected_datetime, DatetimeValidator::validate(given_datetime).unwrap())
+        };
+    }
+}
