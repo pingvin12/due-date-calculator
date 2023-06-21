4488e020734165654aeeb8f208b18a836fb20d3b
pingvin12 <fenyesj@gmail.com>
2023-06-21 19:42:02 +0200

    feat(turnaround): add turnaround and validation

---
+++ src/validator/turnaround.rs
@@ -0,0 +1,17 @@
+use std::{time::SystemTime, str::FromStr};
+
+use chrono::{DateTime, Utc};
+
+use crate::misc::errortypes::validation_error::ValidationError;
+
+pub struct TurnaroundValidator {
+}
+
+impl TurnaroundValidator {
+    pub fn validate(hours: i64) -> Result<(), ValidationError> {
+        if hours < 0 {
+            return Err(ValidationError::InvalidTurnaround { example: "8".to_owned(), found: hours.to_string() })
+        }
+        Ok(())
+    }
+}
---
+++ tests/validator/turnaround_validator_tests.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod turnaround_validator_tests {
+    use due_date_calculator::validator::{turnaround::TurnaroundValidator};
+    use test_case::test_case;
+    #[test_case(8; "Validate turnaround hours with validator, should work successfully")]
+    #[test_case(11;)]
+    #[test_case(1;)]
+    #[test_case(3;)]
+    fn test_turnaround_validator(hours: i64) {
+        let _ = TurnaroundValidator::validate(hours);
+    }
+}
